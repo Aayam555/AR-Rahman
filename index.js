24287f5e30beee3dce69275173505768588e6745
Aayam Shrestha <63101665+Aayam555@users.noreply.github.com>
2022-04-28 15:04:53 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,119 @@
+  // #=======================Songs===========================#
+
+        const songs =
+        [
+    {
+        "name": "Swades: Yun Hi Chala Chal",
+        "link": "https://www.youtube.com/watch?v=eEeX2QMlSlo",
+        "thumbnail": "https://i.ytimg.com/vi/eEeX2QMlSlo/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCo1ton97LS0bRnrzl_OCPFPyLgkw"
+    },
+    {
+        "name": "Bombay: Tu Hi Re",
+        "link": "https://www.youtube.com/watch?v=o0ntND9OZ6I",
+        "thumbnail": "https://i.ytimg.com/vi/o0ntND9OZ6I/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAt_gCszoP7xQEBjE9eUPCgiYErWQ"
+    },
+    {
+        "name": "Bombay: Humma Humma",
+        "link": "https://www.youtube.com/watch?v=7Jojcy2siAo",
+        "thumbnail": "https://i.ytimg.com/vi/7Jojcy2siAo/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAo51Gw13E2NkIo3qChm9uFMpWd-w"
+    },
+    {
+        "name": "Bombay: Kehna Hi Kya",
+        "link": "https://www.youtube.com/watch?v=kW9ynOaDJZg",
+        "thumbnail": "https://i.ytimg.com/vi/kW9ynOaDJZg/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDYQHDGCUVDhyzWYePYZAwgpjiEpg"
+    },
+    {
+        "name": "Dil Se: Dil Se Re",
+        "link": "https://www.youtube.com/watch?v=TcdEOHV3PgA",
+        "thumbnail": "https://i.ytimg.com/vi/TcdEOHV3PgA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBUqyWV8Vo4_nNGbCjcG2F1mAeyJg"
+    },
+    {
+        "name": "Dil Se: Chaiyya Chaiyya",
+        "link": "https://www.youtube.com/watch?v=9yT4F8hzykY",
+        "thumbnail": "https://i.ytimg.com/vi/9yT4F8hzykY/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCWS0vYvmIDi4oVOiTeM8VGQFrqzg"
+    },
+    {
+        "name": "Roja: Dil Hai Chhota Sa",
+        "link": "https://www.youtube.com/watch?v=Wk9GL7zvU24",
+        "thumbnail": "https://i.ytimg.com/vi/Wk9GL7zvU24/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBNv9jrRt_BiGrRurUIbveTFOISxw"
+    },
+    {
+        "name": "Roja: Yeh Haseen Vadiyan",
+        "link": "https://www.youtube.com/watch?v=z-vcE-UTc-0",
+        "thumbnail": "https://i.ytimg.com/vi/z-vcE-UTc-0/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBT-xRiGCE2OyhG5Q_7G-GFI1SOlQ"
+    },
+    {
+        "name": "Atrangi Re: Garda",
+        "link": "youtube.com/watch?v=8lUmJuPvfnw",
+        "thumbnail": "https://i.ytimg.com/vi/8lUmJuPvfnw/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLANPRX5XIbae8PvnYhv8alNB1C_8g"
+    },
+    {
+        "name": "Atrangi Re: Chaka Chak",
+        "link": "https://www.youtube.com/watch?v=Kz5uJlucE5Q",
+        "thumbnail": "https://i.ytimg.com/vi/Kz5uJlucE5Q/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCWg6SP7etb0zYxSZGHw73EqYgggQ"
+    },
+    {
+        "name": "Atrangi Re: Tere Rang",
+        "link": "https://www.youtube.com/watch?v=f8gdO0JePQ0",
+        "thumbnail": "https://i.ytimg.com/vi/f8gdO0JePQ0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC9BHfvR6H-36rLig_A-1aEmCRkpQ"
+    },
+    {
+        "name": "Atrangi Re: Little Little",
+        "link": "https://www.youtube.com/watch?v=Z6rEHTLBGeg",
+        "thumbnail": "https://i.ytimg.com/vi/Z6rEHTLBGeg/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAYJwDCZEXVVogJ0YwpZy2KdqIR7Q"
+    },
+    {
+        "name": "Atrangi Re: Tumhein Mohabbat Hai",
+        "link": "https://www.youtube.com/watch?v=NegGZIlgfNA",
+        "thumbnail": "https://i.ytimg.com/vi/NegGZIlgfNA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCK_lAV0CdxHP3ltbR_vUB9MfCw7w"
+    },
+    {
+        "name": "Atrangi Re: Rait Zara Si ",
+        "link": "https://www.youtube.com/watch?v=XejntCTDZZU",
+        "thumbnail": "https://i.ytimg.com/vi/XejntCTDZZU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCm38PA86Fg7QyC_8VMDTMt09PyDA"
+    },
+    {
+        "name": "Atrangi Re: Toofan Si Kudi",
+        "link": "https://www.youtube.com/watch?v=PseaL6b8FdA",
+        "thumbnail": "https://i.ytimg.com/vi/PseaL6b8FdA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBJ3oivrY4Xv1BkirjyrFL-yyzp9Q"
+    },
+    {
+        "name": "Mimi: Rihaayi De",
+        "link": "https://www.youtube.com/watch?v=5jO9K0rjbt0",
+        "thumbnail": "https://i.ytimg.com/vi/5jO9K0rjbt0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAGFYJ92oB2TzWuu3Rw8I79h6Sq3A"
+    },
+    {
+        "name": "Mimi: Hututu",
+        "link": "https://www.youtube.com/watch?v=JF6hF6QxPMU",
+        "thumbnail": "https://i.ytimg.com/vi/JF6hF6QxPMU/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC77ypyvc8q1LX7BiXH-xZx80brSw"
+    },
+    {
+        "name": "Mimi: Param Sundari",
+        "link": "https://www.youtube.com/watch?v=w4ClQO0FFQg",
+        "thumbnail": "https://i.ytimg.com/vi/w4ClQO0FFQg/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBunoXRoj7EDR2eVHNjtKn4eSn7nw"
+    }
+]
+
+        // #========================================================#
+
+        function addNewSong(name, link, thumbnail) {
+            songs.push({ name: name, link: link, thumbnail: thumbnail });
+            return songs
+        }
+
+        function displaySongs() {
+            let displayTemplate = "";
+
+            for (let i = 0; i < songs.length; i++) {
+                displayTemplate += `<li><img class="thumbnail" src="${songs[i].thumbnail}"><button onclick=playSong("${songs[i].link}")>${songs[i].name}</button></li>`
+            }
+
+            const myUL = document.getElementById("myUL");
+            myUL.innerHTML = displayTemplate;
+        }
+
+        function playSong(link) {
+            open(link)
+        }
+
+        if (document.title == "Add new Song"){} else{ displaySongs();}
